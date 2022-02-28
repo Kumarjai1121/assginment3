e37a095e19c110349beb161f091a5c262c39993a
kumarjai1121 <deepak.arya@knoldus.com>
2022-02-28 11:58:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import Comment from "./Comment";
 function App() {
+  const [data, setData] = useState([]);
+  const [show, setShow] = useState({ postId: null, showComment: false });
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts").then((resp) => {
+      resp.json().then((result) => setData(result));
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="color1">
+      <div id="heading">POSTS {show.showComment.toString()}</div>{" "}
+      {data.map((item) => (
+        <div className="color2" key={item.id}>
+         <div id="title">{item.title}</div>{" "}
+          <div id="body">{item.body}</div>{" "}
+          <div>
+            {" "}
+            <button
+              id="btn"
+              onClick={() =>
+                setShow({ postId: item.id, showComment: !show.showComment })
+              }
+            >
+            {" "}
+              {show.showComment && show.postId === item.id
+                ? "Hide Comment"
+                : "Show Comment"}
+            {" "}
+            </button>
+          {" "}
+            {show.showComment ? (
+              <Comment id={item.id} postId={show.postId} />
+            ) : null}
+          {" "}
+          </div>
+          {" "}
+        </div>
+      ))}
+    {" "}
     </div>
   );
 }
